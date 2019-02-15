48339aeb7fe257383094d8062953306ef0ae5a34
chenxuan <523764720@qq.com>
2019-02-15 15:57:19 +0800

    增加获取默认kms服务器ip的方法

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
-import setting.Key
-import setting.Server
+from setting.Key import Key 
+from setting.Server import Server 
 
 
-print setting.Key.Key()
-print setting.Key.Key()
-print setting.Server.Server()
-print setting.Server.Server()
+print Key().key_list
+print Key()
+print Server().server_list
+print Server().get_default_server()
--- setting/Server.py
+++ setting/Server.py
@@ -8,4 +8,12 @@ class Server(Singleton):
 
     def __Singleton_Init__(self):
         self.server_list = NetGeter.get_json("https://raw.githubusercontent.com/cxwithyxy/weiruanshichuangxitongjihuogongju/master/config/server_list.json")
-        pass
+        
+    def get_default_server(self):
+        for i in self.server_list:
+            try:
+                if i["default"]:
+                    return i["ip"]
+            except:
+                pass
+        return self.server_list[0]["ip"]
