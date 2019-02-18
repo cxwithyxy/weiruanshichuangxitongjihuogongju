d71297e2fa80b8ea34a71e6ef0c8e9c8ef1c7d1e
chenxuan <523764720@qq.com>
2019-02-18 14:36:59 +0800

    完成注册码获取的操作

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+#coding:utf-8
+
 from setting.Key import Key 
 from setting.Server import Server 
 from lib.Systeminfo import Systeminfo 
@@ -11,4 +13,11 @@ sys.setdefaultencoding( "gbk" )
 # print Server().server_list
 # print Server().get_default_server()
 
-print Systeminfo().get_windows_version_name()
+print u"获取系统版本..."
+
+window_version_name = Systeminfo().get_windows_version_name()
+print u"当前版本是: " + window_version_name
+
+print u"获取注册码..."
+reg_code = Key().get_key(window_version_name)
+print u"适配注册码为: " + reg_code
--- setting/Key.py
+++ setting/Key.py
@@ -9,3 +9,6 @@ class Key(Singleton):
     def __Singleton_Init__(self):
         self.key_list = NetGeter.get_json("https://raw.githubusercontent.com/cxwithyxy/weiruanshichuangxitongjihuogongju/master/config/key_list.json")
         pass
+
+    def get_key(self, window_version_name):
+        return self.key_list[window_version_name][0]
