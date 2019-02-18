aba63154db5a3b6cd05683be956bbe19bdd5af2f
chenxuan <523764720@qq.com>
2019-02-18 14:57:36 +0800

    完成注册流程的cmd命令编写

--- index.py
+++ index.py
@@ -4,14 +4,14 @@ from setting.Key import Key
 from setting.Server import Server 
 from lib.Systeminfo import Systeminfo 
 import sys
+import os
 
 reload(sys)
 sys.setdefaultencoding( "gbk" )
 
-# print Key().key_list
-# print Key()
-# print Server().server_list
-# print Server().get_default_server()
+def runCmd(cmd):
+    os.system(cmd)
+    pass
 
 print u"获取系统版本..."
 
@@ -21,3 +21,22 @@ print u"当前版本是: " + window_version_name
 print u"获取注册码..."
 reg_code = Key().get_key(window_version_name)
 print u"适配注册码为: " + reg_code
+
+print u"获取注册服务器..."
+server_ip = Server().get_default_server()
+print u"适配注册服务器为: " + server_ip
+
+# slmgr /upk
+# slmgr /ipk xxxxx-xxxxx-xxxxx-xxxxx-xxxxx（激活密匙）
+# slmgr /skms kms.xspace.in
+# slmgr /ato
+print u"卸载系统已经存在的注册码"
+runCmd("slmgr /upk")
+print u"把注册码 " + reg_code + u" 写入系统"
+runCmd("slmgr /ipk " + reg_code)
+print u"设置注册服务器"
+runCmd("slmgr /skms " + server_ip)
+
+print u"进行注册"
+runCmd("slmgr /ato")
+print u"注册完毕"
