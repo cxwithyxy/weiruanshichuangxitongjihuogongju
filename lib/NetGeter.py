8675f44701be0fd5d70558f7f43b41bad1491886
chenxuan <523764720@qq.com>
2019-04-01 12:01:04 +0800

    修复 cacert.pem 打包路径的问题

--- lib/NetGeter.py
+++ lib/NetGeter.py
@@ -3,8 +3,5 @@
 import requests
 from os.path import join, abspath
 
-requests.utils.DEFAULT_CA_BUNDLE_PATH = join(abspath('.'), 'cacert.pem')
-requests.adapters.DEFAULT_CA_BUNDLE_PATH = join(abspath('.'), 'cacert.pem')
-
 def get_json(url):
     return requests.get(url).json()
