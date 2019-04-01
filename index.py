04319b16b5907fbbec3f455082052979993316d3
chenxuan <523764720@qq.com>
2019-04-01 15:36:49 +0800

    用 python3.7 开发了

--- index.py
+++ index.py
@@ -6,35 +6,32 @@ from lib.Systeminfo import Systeminfo
 import sys
 import os
 
-reload(sys)
-sys.setdefaultencoding( "gbk" )
-
 def runCmd(cmd):
     os.system(cmd)
     pass
 
-print u"获取系统版本..."
+print( u"获取系统版本...")
 
 window_version_name = Systeminfo().get_windows_version_name()
-print u"当前版本是: " + window_version_name
+print( u"当前版本是: " + window_version_name)
 
-print u"获取注册码..."
+print( u"获取注册码...")
 reg_code = Key().get_key(window_version_name)
-print u"适配注册码为: " + reg_code
+print( u"适配注册码为: " + reg_code)
 
-print u"获取注册服务器..."
+print( u"获取注册服务器...")
 server_ip = Server().get_default_server()
-print u"适配注册服务器为: " + server_ip
+print( u"适配注册服务器为: " + server_ip)
 
-print u"卸载系统已经存在的注册码"
+print( u"卸载系统已经存在的注册码")
 runCmd("cscript //nologo c:/Windows/System32/slmgr.vbs /upk")
-print u"把注册码 " + reg_code + u" 写入系统"
+print( u"把注册码 " + reg_code + u" 写入系统")
 runCmd("cscript //nologo c:/Windows/System32/slmgr.vbs /ipk " + reg_code)
-print u"设置注册服务器"
+print( u"设置注册服务器")
 runCmd("cscript //nologo c:/Windows/System32/slmgr.vbs /skms " + server_ip)
 
-print u"进行注册"
+print( u"进行注册")
 runCmd("cscript //nologo c:/Windows/System32/slmgr.vbs /ato")
-print u"注册完毕"
+print( u"注册完毕")
 
-raw_input(u"按下回车键结束运行")
+input(u"按下回车键结束运行")
--- lib/Systeminfo.py
+++ lib/Systeminfo.py
@@ -10,7 +10,7 @@ class Systeminfo(Singleton):
     
     def __Singleton_Init__(self):
         output = os.popen('systeminfo')
-        self.info_text = output.read().decode("gbk")
+        self.info_text = output.read()
 
     def get_windows_version_name(self):
         matchObj = re.search( u'OS 名称.*$', self.info_text, re.M)
